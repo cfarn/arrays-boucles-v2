9e875ba4673f2d0edfe5d1563d17f0a4ea83ccb6
cfarn <58476975+cfarn@users.noreply.github.com>
2022-06-23 12:22:36 +0200

    ex 2

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const log = console.log
 
 // 1 - Nombres pairs
+// let total = 0
+// for(let i = 11; i <47; i++) {
+//     // log(i)
+//     if(i % 2 === 0) {
+//         log(i)
+//     }
+//     total += i
+// }
+// log(total)
+
+// 2 - Nombres impairs
 let total = 0
-for(let i = 11; i <47; i++) {
+for(let i = 109; i <588; i++) {
     // log(i)
-    if(i % 2 === 0) {
+    if(i % 2 !== 0) {
         log(i)
     }
     total += i
