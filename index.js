915353738707c828d0745a8e3cc96e2fdd418bcc
cfarn <58476975+cfarn@users.noreply.github.com>
2022-07-24 15:19:58 +0200

    maj

--- index.js
+++ index.js
@@ -12,12 +12,21 @@ const log = console.log
 // log(total)
 
 // 2 - Nombres impairs
-let total = 0
-for(let i = 109; i <588; i++) {
-    // log(i)
-    if(i % 2 !== 0) {
-        log(i)
-    }
-    total += i
+// let total = 0
+// for(let i = 109; i <588; i++) {
+//     // log(i)
+//     if(i % 2 !== 0) {
+//         log(i)
+//     }
+//     total += i
+// }
+// log(total)
+
+// 3 - Puissance et racine carrée
+const numbers = [1, 2, 3, 4, 5]
+const squares = []
+const roots = []
+
+for(let i = 0; i < numbers.length; i++) {
+    log (numbers[i])
 }
-log(total)
